a5990bc462cfe1cae0c8d07cd8e285b07df3beef
yrajaram <yrajaram@heraklescorp.com>
2017-06-03 17:32:18 -0700

    Create Main.java

---
+++ src/com/herakles/test/Main.java
@@ -0,0 +1,6 @@
+/*
+ * This is a linear order sort algorithm which depends on the maximum element and the minimum element in the input. 
+ * For example if the maximum value is 100 then this will perform in O(100) irrespective of the number of elements in the input array.
+ * Contrast this will Bubble Sort which with perform in O(100) if 10 elements are in the input array and will be O(400) if 20 elements
+ * are in the input.
+ */
